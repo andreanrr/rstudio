3ff8e9e3516545ec5b2e466417c11477cd7a687b
isabel.rivera5 <isabel.rivera5@upr.edu>
2020-09-25 07:33:42 -0400

    inflammation file to execute functions written in inflammation_fxns.R

---
+++ Chapter 6/CopyOfinflammation.R
@@ -0,0 +1,8 @@
+##chapter 6
+
+## This script calls the functions written in inflammation_fxns.R
+
+##Make sure both rscripts are in the same directory as the files you will analyze
+source("inflammation_fxns.R")
+analyze_all("inflammation-")
+
